1b6458dedb37fa390b56b358dfa57881dde9336b
UliWT <wojciktulaulises@gmail.com>
2025-02-09 22:01:56 -0300

    menu arreglado con sesiones

--- front/User/menu.php
+++ front/User/menu.php
@@ -1,3 +1,19 @@
+<?php
+// Iniciar sesión
+session_start();
+
+// Verificar si el usuario está autenticado
+if (!isset($_SESSION['id_usuario'])) {
+    // Si el usuario no está autenticado, redirigir a la página de inicio de sesión
+    header("Location: ../Login/Login.html");
+    exit();
+}
+
+// Obtener el ID del usuario de la sesión
+$id_usuario = $_SESSION['id_usuario'];
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
