30d08bafc17952a7087a6c6a85441ccbfa6c6fef
Leonardo Cangialosi <leocangialosi04@gmail.com>
2025-02-09 21:50:44 -0300

    Front Menu, buttons, navbar, home & logout

--- front/User/menu.php
+++ front/User/menu.php
@@ -4,24 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menú de Usuario</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Conexión con CSS -->
+    <link rel="stylesheet" href="styles-menu.css"> <!-- Conexión con CSS -->
+    <!-- FontAwesome para íconos -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
 
+<!-- Navbar -->
 <div class="navbar">
-    <a href="index.php">Inicio</a>
-    
-    <div class="dropdown">
-        <a href="#">Turnos ▼</a>
-        <div class="dropdown-content">
-            <a href="usuario_v.php">Registrar un Turno</a>
-            <a href="mis_turnos.php">Ver mis Turnos</a>
-        </div>
+    <a href="index.php">
+        <i class="fas fa-home"></i> Inicio
+    </a>
+    <form action="../Logout/logout.php" method="POST">
+        <button type="submit">
+            <i class="fas fa-sign-out-alt"></i> Cerrar Sesión
+        </button>
+    </form>
+</div>
+
+<!-- Contenedor de las opciones con imágenes -->
+<div class="container">
+    <div class="option" id="Block1">
+        <a href="usuario_v.php">
+            <img src="registrar.png" alt="Registrar un Turno">
+            <p>Registrar un Turno</p>
+        </a>
     </div>
 
-    <form action="../Logout/logout.php" method="POST">
-        <button type="submit">Cerrar Sesión</button>
-    </form>
+    <div class="option" id="Block2">
+        <a href="mis_turnos.php">
+            <img src="ver.png" alt="Ver mis Turnos">
+            <p>Ver mis Turnos</p>
+        </a>
+    </div>
 </div>
 
 </body>
